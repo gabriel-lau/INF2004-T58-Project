8143bbfa1611e23d0220aafa2da57010b873235d
keaganwongkl <44427454+keaganwongkl@users.noreply.github.com>
2023-10-30 21:46:31 +0800

    IR Line Sensor (Line following)

---
+++ driver/irline/irline.c
@@ -0,0 +1,35 @@
+#include <stdio.h>  // Include the standard input/output header
+#include "pico/stdlib.h"
+
+//A0 = 16, AGND = GND, VCC to 3.3 (3 above GND)
+
+
+int main() {
+    // Initialize the Pico SDK
+    stdio_init_all();
+
+    // Configure the GPIO pin connected to the line tracking sensor
+    const uint lineTrackingPin = 16;  // Change this to your sensor's pin
+
+    gpio_init(lineTrackingPin);
+    gpio_set_dir(lineTrackingPin, GPIO_IN);
+
+    while (1) {
+        if (gpio_get(lineTrackingPin)==0) {
+            // The line tracking sensor detected a line
+            printf("Value of 0 detected\n");
+        }else if (gpio_get(lineTrackingPin)==1)
+        {
+            printf("Value of 1 detected\n");
+        }
+         else {
+            // The line tracking sensor didn't detect a line
+            printf("Line detected\n");
+        }
+
+        // Add a delay to control the update rate
+        sleep_ms(100);  // Adjust the delay as needed
+    }
+
+    return 0;
+}
