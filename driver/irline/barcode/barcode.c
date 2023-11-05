1fe21d9d45de7f42ef49f13d8bb2176e76da4a43
Shi Ya <64403759+osshiya@users.noreply.github.com>
2023-11-06 02:01:44 +0800

    Barcode only (#1)
    * updates
    
    * up
    
    * Update CMakeLists.txt
    
    * fix

--- driver/irline/barcode/barcode.c
+++ driver/irline/barcode/barcode.c
@@ -1,150 +1,186 @@
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/gpio.h"
-#include "hardware/adc.h"
 #include "pico/time.h"
 
-// Constants
-#define BARCODE_SENSOR_PIN 26
-#define BARCODE_ADC_CHANNEL 0
-#define BARCODE_THRESHOLD 3000
+#include "FreeRTOS.h"
+#include "task.h"
 
-// Global Variables
-static int black_bar_times[5] = {0, 0, 0, 0, 0};
-static int white_bar_times[5] = {0, 0, 0, 0, 0};
-static int bar_index = 0;
-static bool isPrevBlackBar = false;
-static uint64_t last_button_press_time = 0;
+#include "hardware/gpio.h"
+#include "hardware/pwm.h"
+#include "hardware/adc.h"
+#include "hardware/clocks.h"
 
-// Code 39 Characters and Patterns
-const char CODE39_CHARS[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-. $/+%*";
-const char *CODE39_PATTERNS[] = {
-    "NNWWN",  // 0
-    "WNNNW",  // 1
-    "NWNNW",  // 2
-    "WWNNN",  // 3
-    "NNWNW",  // 4
-    "WNWNN",  // 5
-    "NWWNN",  // 6
-    "NNNWW",  // 7
-    "WNNWN",  // 8
-    "NWNWN",  // 9
-    "WNNNW",  // A
-    "NNWNNWNNW",  // B
-    "WNWNNWNNN",  // C
-    "NNNNWWNNW",  // D
-    "WNNNWWNNN",  // E
-    "NNWNWWNNN",  // F
-    "NNNNNWWNW",  // G
-    "WNNNNWWNN",  // H
-    "NNWNNWWNN",  // I
-    "NNNNWWWNN",  // J
-    "WNNNNNNWW",  // K
-    "NNWNNNNWW",  // L
-    "WNWNNNNWN",  // M
-    "NNNNWNNWW",  // N
-    "WNNNWNNWN",  // O
-    "NNWNWNNWN",  // P
-    "NNNNNNWWW",  // Q
-    "WNNNNNWWN",  // R
-    "NNWNNNWWN",  // S
-    "NNNNWNWWN",  // T
-    "WWNNNNNNW",  // U
-    "NWWNNNNNW",  // V
-    "WWWNNNNNN",  // W
-    "NWNNWNNNW",  // X
-    "WWNNWNNNN",  // Y
-    "NWWNWNNNN",  // Z
-    "NWNNNNWNW",  // -
-    "WWNNNNWNN",  // .
-    "NWWNNNWNN",  // SPACE
-    "NWNWNWNNN",  // $
-    "NWNWNNNWN",  // /
-    "NWNNNWNWN",  // +
-    "NNNWNWNWN",  // %
-    "NNWWN",  // *
-};
+#include "barcode.h"
 
-// Function to get IR reading
-int get_ir_reading() {
-    uint16_t reading = adc_read();
-    return reading;
+volatile bool isBlackBar = false;
+static uint64_t lastBarTime = 0;
+
+BarcodeState currentState = STATE_WAITING_FOR_START;
+static int blackBarCount = 0;
+static int whiteBarCount = 0;
+static int done_black = 0;
+static int done_white = 0;
+
+#define MAX_PULSE_WIDTHS 9                      // Adjust this to your desired array size
+static int pulseWidthCount = 0;                 // Counter for the number of stored pulseWidth values
+static uint64_t pulseWidths[MAX_PULSE_WIDTHS];  // Array to store pulseWidth values
+
+int get_ir_reading()
+{
+  uint16_t reading = adc_read();
+  return reading;
 }
 
-// Function to decode barcode pattern
-char decodeBarcodePattern(int black_bar_times[], int white_bar_times[]) {
-    char pattern[6];
-    int total_time = 0;
-    for (int i = 0; i < 5; i++) {
-        total_time += black_bar_times[i] + white_bar_times[i];
-    }
-    int avg_time = total_time / 10;  // Average time for one bar
-
-    for (int i = 0; i < 5; i++) {
-        pattern[i] = (black_bar_times[i] > avg_time) ? 'W' : 'N';
-    }
-    pattern[5] = '\0';
-
-    printf("Trying to decode pattern: %s\n", pattern);  // Debug print
-
-    for (int i = 0; i < sizeof(CODE39_CHARS) - 1; i++) {
-        if (strcmp(pattern, CODE39_PATTERNS[i]) == 0) {
-            return CODE39_CHARS[i];
+// Function to find the Code 39 character for a given barcode pattern
+char findCode39Character(const char* pattern) {
+    int numMappings = sizeof(barcodeMappings) / sizeof(barcodeMappings[0]);
+    for (int i = 0; i < numMappings; i++) {
+        if (strcmp(barcodeMappings[i].pattern, pattern) == 0) {
+            return barcodeMappings[i].code39_character;
         }
     }
-    return '?';  // Unknown pattern
+    return '?'; // Character not found
+}
+
+void detect_barcode() {
+  int reading = get_ir_reading();
+  int currentTime = time_us_64();
+  int pulseWidth = currentTime - lastBarTime;
+
+  switch (currentState) {
+    case STATE_WAITING_FOR_START:
+      if (!isBlackBar && reading > BARCODE_BLACK_THRESHOLD) {
+        // Detected the start signal (black bar)
+        currentState = STATE_DETECTING_WHITE;
+        isBlackBar = true;
+        lastBarTime = currentTime;
+        blackBarCount = 1;
+      }
+    break;
+
+    case STATE_DETECTING_BLACK:
+      if (!isBlackBar && reading > BARCODE_BLACK_THRESHOLD) {
+        // Detected another black bar
+        currentState = STATE_DETECTING_WHITE;
+        isBlackBar = true;
+        lastBarTime = currentTime;
+        blackBarCount++;
+        pulseWidths[pulseWidthCount++] = pulseWidth; // Store pulseWidth
+        printf("Detected White Bar - Pulse Width: %d\n", pulseWidth);
+        if (blackBarCount > BLACK_BAR_COUNT) {
+          printf("Barcode Overload!\n");
+        }
+      }
+    break;
+
+    case STATE_DETECTING_WHITE:
+      if (isBlackBar && reading < BARCODE_WHITE_THRESHOLD) {
+        // Detected a white bar
+        isBlackBar = false;
+        lastBarTime = currentTime;
+        whiteBarCount++;
+        pulseWidths[pulseWidthCount++] = pulseWidth; // Store pulseWidth
+        printf("Detected Black Bar - Pulse Width: %d\n", pulseWidth);
+        if (whiteBarCount > WHITE_BAR_COUNT) {
+          // Successfully detected the Code 39 pattern
+          printf("Code 39 Pattern Detected!\n");
+          // Calculate the average pulse width
+          uint64_t averagePulseWidth = 0;
+          for (int i = 0; i < pulseWidthCount; i++) {
+              averagePulseWidth += pulseWidths[i];
+          }
+          averagePulseWidth /= pulseWidthCount;
+
+          // Set a threshold to differentiate narrow and wide bars
+          uint64_t threshold = averagePulseWidth;
+
+          // Initialize an array to store narrow (0) or wide (1) bar classifications
+          int barcodeClassification[pulseWidthCount];
+
+          for (int i = 0; i < pulseWidthCount; i++) {
+              if (pulseWidths[i] > threshold) {
+                  barcodeClassification[i] = 1;  // Wide bar
+              } else {
+                  barcodeClassification[i] = 0;  // Narrow bar
+              }
+          }
+
+          char barcodePattern[MAX_PULSE_WIDTHS] = ""; // Initialize an empty string
+          char str[1];
+
+          for (int i = 0; i < pulseWidthCount; i++) {
+            sprintf(str, "%d", barcodeClassification[i]);
+            strcat(barcodePattern, str);
+          }
+
+          char code39Character = findCode39Character(barcodePattern);
+          printf("Barcode Pattern: %s => Code 39 Character: %c\n", barcodePattern, code39Character);
+
+
+          // Reset the FSM to wait for the next barcode
+          blackBarCount = 0;
+          whiteBarCount = 0;
+          pulseWidthCount = 0;
+          currentState = STATE_WAITING_FOR_START;
+        } else {
+          currentState = STATE_DETECTING_BLACK;
+        }
+      }
+      break;
+  }
 }
 
 
-// Function to read barcode
-void readBarcode() {
-    uint16_t reading = get_ir_reading();
+void read_barcode(__unused void *params) {
+    gpio_init(BARCODE_SENSOR_PIN);
+    gpio_set_dir(BARCODE_SENSOR_PIN, GPIO_IN);
 
-     printf("ADC Reading: %d\n", reading);  // Print ADC reading
+    adc_init();
+    adc_gpio_init(BARCODE_SENSOR_PIN);    
+    adc_select_input(BARCODE_ADC_CHANNEL);  
 
-    if (reading > BARCODE_THRESHOLD && !isPrevBlackBar) {
-        isPrevBlackBar = true;
-        int timing = time_us_64() - last_button_press_time;
-        black_bar_times[bar_index] = timing;
-    } else if (reading < BARCODE_THRESHOLD && isPrevBlackBar) {
-        isPrevBlackBar = false;
-        int timing = time_us_64() - last_button_press_time;
-        white_bar_times[bar_index] = timing;
-        bar_index++; 
+    while(true) {
+      detect_barcode();
     }
+}
+void vLaunch(void) {
+  TaskHandle_t barcode_task;
+  xTaskCreate(read_barcode, "BarcodeThread", configMINIMAL_STACK_SIZE, NULL, 1, &barcode_task);
 
-    if (white_bar_times[4] != 0) {
-        printf("Black bar times: ");
-        for (int i = 0; i < 5; i++) {
-            printf("%d ", black_bar_times[i]);
-        }
-        printf("\n");
+#if NO_SYS && configUSE_CORE_AFFINITY && configNUM_CORES > 1
+  // we must bind the main task to one core (well at least while the init is called)
+  // (note we only do this in NO_SYS mode, because cyw43_arch_freertos
+  // takes care of it otherwise)
+  vTaskCoreAffinitySet(task, 1);
+#endif
 
-        printf("White bar times: ");
-        for (int i = 0; i < 5; i++) {
-            printf("%d ", white_bar_times[i]);
-        }
-        printf("\n");
-
-        char decodedChar = decodeBarcodePattern(black_bar_times, white_bar_times);
-        printf("Decoded character: %c\n", decodedChar);
-
-        // Reset variables for next read
-        isPrevBlackBar = false;
-        white_bar_times[4] = 0;
-        bar_index = 0;
-    }
+  /* Start the tasks and timer running. */
+  vTaskStartScheduler();
 }
 
 int main(void) {
+    // Initialize the Pico SDK
     stdio_init_all();
-    adc_init();
-    adc_gpio_init(BARCODE_SENSOR_PIN);
-    adc_select_input(BARCODE_ADC_CHANNEL);
 
-    while (1) {
-        readBarcode();
-        sleep_ms(500);
-    }
+    /* Configure the hardware ready to run the demo. */
+    const char *rtos_name;
+    #if ( portSUPPORT_SMP == 1 )
+        rtos_name = "FreeRTOS SMP";
+    #else
+        rtos_name = "FreeRTOS";
+    #endif
+
+    #if ( portSUPPORT_SMP == 1 ) && ( configNUM_CORES == 2 )
+        printf("Starting %s on both cores:\n", rtos_name);
+        vLaunch();
+    #elif ( RUN_FREERTOS_ON_CORE == 1 )
+        printf("Starting %s on core 1:\n", rtos_name);
+        multicore_launch_core1(vLaunch);
+        while (true);
+    #else
+        printf("Starting %s on core 0:\n", rtos_name);
+        vLaunch();
+    #endif
+        return 0;
 }
---
+++ driver/irline/barcode/barcode.h
@@ -0,0 +1,70 @@
+#ifndef BARCODE_H
+#define BARCODE_H
+
+// Configure the GPIO pin connected to the line tracking sensor
+#define BARCODE_SENSOR_PIN 26  // Change this to your sensor's pin
+#define BARCODE_ADC_CHANNEL 0  // Change this to your sensor's adc channel
+#define BARCODE_BLACK_THRESHOLD 1200
+#define BARCODE_WHITE_THRESHOLD 1000
+#define BLACK_BAR_COUNT 5
+#define WHITE_BAR_COUNT 4
+
+// Define the states of the FSM
+typedef enum {
+    STATE_WAITING_FOR_START,  // Waiting for the start signal
+    STATE_DETECTING_BLACK,    // Detecting a black bar
+    STATE_DETECTING_WHITE,    // Detecting a white bar
+} BarcodeState;
+
+// Structure to represent the mapping
+typedef struct {
+    const char* pattern;
+    char code39_character;
+} BarcodeMapping;
+
+// Define an array of barcode-to-Code 39 mappings
+BarcodeMapping barcodeMappings[] = {
+    { "000110100", '0' },
+    { "100100001", '1' },
+    { "001100001", '2' },
+    { "101100000", '3' },
+    { "000110001", '4' },
+    { "100110000", '5' },
+    { "001110000", '6' },
+    { "000100101", '7' },
+    { "100100100", '8' },
+    { "001100100", '9' },
+    { "100001001", 'A' },
+    { "001001001", 'B' },
+    { "101001000", 'C' },
+    { "000011001", 'D' },
+    { "100011000", 'E' },
+    { "001011000", 'F' },
+    { "000001101", 'G' },
+    { "100001100", 'H' },
+    { "001001100", 'I' },
+    { "100000011", 'J' },
+    { "001000011", 'K' },
+    { "101000010", 'L' },
+    { "000010011", 'M' },
+    { "100010010", 'N' },
+    { "001010010", 'O' },
+    { "000000111", 'P' },
+    { "100000110", 'Q' },
+    { "001000110", 'R' },
+    { "000010110", 'S' },
+    { "100010100", 'T' },
+    { "001010100", 'U' },
+    { "100110001", 'V' },
+    { "001110001", 'W' },
+    { "000100111", 'X' },
+    { "100100110", 'Y' },
+    { "001100110", 'Z' },
+    { "010010100", '*' },
+    { "010001001", '-' },
+    { "001001010", '.' },
+    { "010101000", ' ' }
+};
+
+
+#endif
