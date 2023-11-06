93dd84118ea9e06c7353c551de326f631ba58f0a
Shi Ya <64403759+osshiya@users.noreply.github.com>
2023-11-06 18:09:18 +0800

    cmake and wifi (#2)

---
+++ driver/wifi/http_server.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "lwip/apps/httpd.h"
+#include "lwipopts.h"
+#include "http_server.h"
+#include "ssi.h"
+#include "cgi.h"
+
+// WIFI Credentials - take care if pushing to github!
+// const char WIFI_SSID[] = "";
+// const char WIFI_PASSWORD[] = "";
+
+void run_http_server() {
+    // Initialise web server
+    httpd_init();
+    extern cyw43_t cyw43_state;
+    int ip_addr = cyw43_state.netif[CYW43_ITF_STA].ip_addr.addr;
+    printf("HTTP Server initialised\n");
+    printf("Starting http server at %lu.%lu.%lu.%lu\n\n", ip_addr & 0xFF, (ip_addr >> 8) & 0xFF, (ip_addr >> 16) & 0xFF, ip_addr >> 24);
+
+    // Configure SSI and CGI handler
+    ssi_init(); 
+    printf("SSI Handler initialised\n");
+    cgi_init();
+    printf("CGI Handler initialised\n");
+    printf("\n");
+
+    // // Infinite loop
+    // // for (;;);
+    // while (1){
+    //     // fgets(console_input, sizeof(console_input), stdin);
+    //     // printf("entered: %s", console_input);
+    // };  
+
+}
