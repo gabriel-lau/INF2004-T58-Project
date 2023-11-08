98c5d972834f405353008c2aae45beef08576146
gabriellau- <40619564+gabriel-lau@users.noreply.github.com>
2023-11-08 13:23:55 +0800

    Added motor funcitons to main.c

--- main.c
+++ main.c
@@ -3,54 +3,59 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
+#include <stdio.h>
+#include <math.h>
 
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 
-#include "lwip/ip4_addr.h"
-
 #include "FreeRTOS.h"
 #include "task.h"
-#include "ping.h"
+#include "message_buffer.h"
+#include "queue.h"
 
-#ifndef PING_ADDR
-#define PING_ADDR "142.251.35.196"
-#endif
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+#include "hardware/timer.h"
+#include "hardware/pwm.h"
+
+#include "driver/motor/motor.h"
+
+#define mbaTASK_MESSAGE_BUFFER_SIZE       ( 60 )
+
 #ifndef RUN_FREERTOS_ON_CORE
 #define RUN_FREERTOS_ON_CORE 0
 #endif
 
 #define TEST_TASK_PRIORITY				( tskIDLE_PRIORITY + 1UL )
 
-void main_task(__unused void *params) {
-    if (cyw43_arch_init()) {
-        printf("failed to initialise\n");
-        return;
-    }
-    cyw43_arch_enable_sta_mode();
-    printf("Connecting to Wi-Fi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-        printf("failed to connect.\n");
-        exit(1);
-    } else {
-        printf("Connected.\n");
-    }
+// GPIO pins for MOTOR
+int INPUT_1_LEFT = 12;
+int INPUT_2_LEFT = 13;
+int PWM_LEFT = 14;
+int INPUT_1_RIGHT = 19;
+int INPUT_2_RIGHT = 18;
+int PWM_RIGHT = 17;
+uint16_t PWM_LEFT_CYCLE = 32768;
+uint16_t PWM_RIGHT_CYCLE = 32768;
 
-    ip_addr_t ping_addr;
-    ipaddr_aton(PING_ADDR, &ping_addr);
-    ping_init(&ping_addr);
+// GPIO pins for ENCODER
+const int ENCODER_LEFT = 15;
+const int ENCODER_RIGHT = 16;
 
-    while(true) {
-        // not much to do as LED is in another task, and we're using RAW (callback) lwIP API
-        vTaskDelay(100);
-    }
+// GPIO pins for MAGNOMETER
+const int MAG_SDA = 0;
+const int MAG_SCL = 1;
 
-    cyw43_arch_deinit();
-}
+// GPIO pins for ULTRASONIC
+const int TRIGGER_PIN = 2;
+const int ECHO_PIN = 3;
+
+// GPIO pins for IR
+
 
 void vLaunch( void) {
-    TaskHandle_t task;
-    xTaskCreate(main_task, "TestMainThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &task);
+    moveForward();
 
 #if NO_SYS && configUSE_CORE_AFFINITY && configNUM_CORES > 1
     // we must bind the main task to one core (well at least while the init is called)
@@ -63,10 +68,43 @@ void vLaunch( void) {
     vTaskStartScheduler();
 }
 
+void motorSetup()
+{
+        //Init Left GPIO
+    gpio_init(INPUT_1_LEFT);
+    gpio_init(INPUT_2_LEFT);
+    gpio_init(PWM_LEFT);
+
+    //Init Right GPIO
+    gpio_init(INPUT_1_RIGHT);
+    gpio_init(INPUT_2_RIGHT);
+    gpio_init(PWM_RIGHT);
+
+    //Set Left GPIO to out power board
+    gpio_set_dir(INPUT_1_LEFT, GPIO_OUT);
+    gpio_set_dir(INPUT_2_LEFT, GPIO_OUT);
+    gpio_set_function(PWM_LEFT, GPIO_FUNC_PWM);
+
+    //Set Right GPIO to out power board
+    gpio_set_dir(INPUT_1_RIGHT, GPIO_OUT);
+    gpio_set_dir(INPUT_2_RIGHT, GPIO_OUT);
+    gpio_set_function(PWM_RIGHT, GPIO_FUNC_PWM);
+
+    // Find out which PWM slice is connected to GPIO
+    uint slice_num_left = pwm_gpio_to_slice_num(PWM_LEFT);
+    uint slice_num_right = pwm_gpio_to_slice_num(PWM_RIGHT);
+
+    pwm_set_gpio_level(PWM_LEFT, PWM_LEFT_CYCLE);
+    pwm_set_gpio_level(PWM_RIGHT, PWM_RIGHT_CYCLE);
+
+    pwm_set_enabled(slice_num_left, true);
+    pwm_set_enabled(slice_num_right, true);
+}
+
 int main( void )
 {
     stdio_init_all();
-
+    void motorSetup();
     /* Configure the hardware ready to run the demo. */
     const char *rtos_name;
 #if ( portSUPPORT_SMP == 1 )
