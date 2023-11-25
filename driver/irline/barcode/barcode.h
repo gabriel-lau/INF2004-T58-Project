e8036ebc475cdd55c1898ce33a40282d6011e0ed
joycexzz <87293250+joycexzz@users.noreply.github.com>
2023-11-25 13:45:15 +0800

    Barcode refractor  (#4)
    * updated variable names to be in camelCase
    
    * updated the barcode
    
    * *
    
    * updated variable names to be in camelCase
    
    * updated the barcode
    
    * Merge branch 'Joyce' of https://github.com/gabriel-lau/INF2004-T58-Project into Joyce
    
    * updated variable names to be in camelCase
    
    * updated the barcode
    
    * clean
    
    * fix reset
    
    ---------
    
    Co-authored-by: osshiya <osshiya@gmail.com>
    Co-authored-by: Shi Ya <64403759+osshiya@users.noreply.github.com>

--- driver/irline/barcode/barcode.h
+++ driver/irline/barcode/barcode.h
@@ -1,6 +1,13 @@
 #ifndef BARCODE_H
 #define BARCODE_H
 
+// every barcode char is 9 bars
+// start w black end with black
+// 5 black bar (+0-9)
+// 4 white bar (+0,+10,+20,+30)
+// narrow = 0
+// thick = 1
+
 // Define GPIO pin and ADC channel for the "front" IR sensor
 #define BARCODE_SENSOR_PIN 26
 #define WALL_SENSOR_PIN 22
@@ -11,17 +18,16 @@
 #define BARCODE_THRESHOLD 1500
 #define BARCODE_CHAR_LIMIT 2
 
-#define mbaTASK_MESSAGE_BUFFER_SIZE (60)
+void initialiseBarcodeReader();
+int captureInfraredSensorReading();
+void handleSensorInterrupt();
+void startBarcodeReadTask();
+void readScannedBarcode();
+void resetReadingParameters();
+void resetCheckParameters();
+void convertBarcodeToCharacter();
 
-// void barcode_init();
-int get_ir_reading();
-void check_if_wall();
-char init_read_barcode();
-void reset_barcode_params();
-char barcode_to_char();
-char decode_barcode(int black_bar_times[], int white_bar_times[]);
-
-enum BarType
+enum currentBarType
 {
     BLACK_BAR,
     WHITE_BAR
@@ -29,23 +35,25 @@ enum BarType
 
 struct Flags
 {
-    bool isPrevBlackBar;
-    bool isBarcode;
-    int count;
-    int limitter;
+    bool isPreviousBlackBarDetected;
+    bool isBarcodeDetected;
+    int wallDetectionCount;
+    int readingLimitReached;
 };
 
 static struct Flags barcodeFlags;
-static enum BarType barType;
-static uint64_t last_button_press_time = 0;
+static enum currentBarType currentBarType;
+static uint64_t timeOfLastDetectedBar = 0;
 static uint16_t coded_barcode = 0;
 static uint64_t decoded_barcode = 0;
 
+extern bool decoded_complete;
+extern char decoded_char;
+
 static int black_bar_times[] = {0, 0, 0, 0, 0}; // Array for black bar times
-static int white_bar_times[] = {0, 0, 0, 0, 0};    // Array for white bar times
-static int bar_index = 0;                          // Index for the current bar
+static int white_bar_times[] = {0, 0, 0, 0, 0}; // Array for white bar times
+static int currentBarIndex = 0;                 // Index for the current bar
 
 static char code_39_characters[] = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ-. *"; // Array of characters for code 39
-
 
 #endif // BARCODE_H
